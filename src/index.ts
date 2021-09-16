1f7bc5aa169acc3b9a5147aa8b8d8affc9ddcfff
Muhammad Bhaska <muhammadbhaska.works@gmail.com>
2021-09-16 07:35:51 +0700

    feat: initialize rollup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const enum EnumTest {
+  VALUE_1 = 1,
+  VALUE_2 = 2,
+}
+
+const test = (): string => {
+  const abc = [EnumTest.VALUE_1, EnumTest.VALUE_2];
+  if (abc[0] === EnumTest.VALUE_1) {
+    return "test1";
+  } else {
+    return "test2";
+  }
+};
+
+console.log(test);
